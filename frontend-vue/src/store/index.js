c92d7cad33d1d47f2cd7f06ef2dee07ec7ccdca6
Fraser Watt <fsrwatt@gmail.com>
2022-06-06 00:13:44 +0100

    Fixed axios commands

--- frontend-vue/src/store/index.js
+++ frontend-vue/src/store/index.js
@@ -33,63 +33,30 @@ export default createStore({
     updateFilter({ commit }, event) {
       commit('updateFilter', event.target.id);
     },
-    getTasksFromBackend({ commit }) {
-      axios.get('https://vuengo-todo-list-backend.herokuapp.com/api/')
-        .then((response) => commit('updateTasks', response.data));
+    async getTasksFromBackend({ commit }) {
+      const response = await axios.get('https://vuengo-todo-list-backend.herokuapp.com/api/');
+      commit('updateTasks', response.data);
     },
     toggleTaskStatus({ state, commit }, taskId) {
       const index = state.tasks.findIndex((x) => x.id === taskId);
-      axios({
-        method: 'put',
-        url: `${process.env.VUE_APP_API_URL}${taskId}/`,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        auth: {
-          username: process.env.VUE_APP_USERNAME,
-          password: process.env.VUE_APP_PASSWORD,
-        },
-        data: {
-          status: (state.tasks[index].status === 'todo') ? 'done' : 'todo',
-          description: state.tasks[index].description,
-        },
+      axios.put(`https://vuengo-todo-list-backend.herokuapp.com/api/${taskId}/`, {
+        status: (state.tasks[index].status === 'todo') ? 'done' : 'todo',
+        description: state.tasks[index].description,
       }).then((response) => commit('updateTaskStatus', response.data));
     },
     deleteTask({ state }, taskId) {
       this.dispatch('getTasksFromBackend');
       if (state.tasks.map((x) => x.id).includes(taskId)) {
-        axios({
-          method: 'delete',
-          url: `${process.env.VUE_APP_API_URL}${taskId}/`,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          auth: {
-            username: process.env.VUE_APP_USERNAME,
-            password: process.env.VUE_APP_PASSWORD,
-          },
-        }).then(() => {
-          this.dispatch('getTasksFromBackend');
-        });
+        axios.delete(`https://vuengo-todo-list-backend.herokuapp.com/api/${taskId}/`)
+          .then(this.dispatch('getTasksFromBackend'));
       }
     },
     clearCompleted({ state }) {
       this.dispatch('getTasksFromBackend');
       state.tasks.forEach((task) => {
         if (task.status === 'done') {
-          axios({
-            method: 'delete',
-            url: `${process.env.VUE_APP_API_URL}${task.id}/`,
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            auth: {
-              username: process.env.VUE_APP_USERNAME,
-              password: process.env.VUE_APP_PASSWORD,
-            },
-          }).then(() => {
-            this.dispatch('getTasksFromBackend');
-          });
+          axios.delete(`https://vuengo-todo-list-backend.herokuapp.com/api/${task.id}/`)
+            .then(this.dispatch('getTasksFromBackend'));
         }
       });
     },
@@ -98,11 +65,7 @@ export default createStore({
       if (taskName) {
         axios({
           method: 'post',
-          url: process.env.VUE_APP_API_URL,
-          auth: {
-            username: process.env.VUE_APP_USERNAME,
-            password: process.env.VUE_APP_PASSWORD,
-          },
+          url: 'https://vuengo-todo-list-backend.herokuapp.com/api/',
           data: {
             description: taskName,
             status: 'todo',
